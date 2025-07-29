108d1805eb3a7377128fbc518698a1530fd26b59
Iryna <irasokolova25@ukr.net>
2025-07-29 21:57:44 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import React from "react";
+import { BrowserRouter, RouterProvider } from "react-router-dom";
+import { routing } from "./router/router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<>
-<React.StrictMode>
-   <App/>
+   <React.StrictMode>
+      <RouterProvider router={routing}>
+            <BrowserRouter basename="/graft/">
+         <App />
+      </BrowserRouter>
+      </RouterProvider>
 </ React.StrictMode>
 </>
 );
