c0a2ed3cc55744bc9cfc8979312d52a84a443773
Iryna <irasokolova25@ukr.net>
2025-07-29 22:17:53 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// export default defineConfig({
-// 	plugins: [react()],
-// 	base: '/',
-// })
 export default defineConfig({
 	plugins: [react()],
 	base: '/graft/',
-	esbuild: {
-		loader: 'jsx',
-	},
-	resolve: {
-		alias: {
-			'./runtimeConfig': './runtimeConfig.browser',
-		},
-	},
-	optimizeDeps: {
-		esbuildOptions: {
-			loader: {
-				'.js': 'jsx',
-			},
-		},
-	},
 })
